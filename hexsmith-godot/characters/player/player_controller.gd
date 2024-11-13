1d483676daefe42fcdf802c5e5783691527e75fc
SebboScruff <36053779+SebboScruff@users.noreply.github.com>
2024-11-13 18:08:05 +0000

    had to source a different player sprite that was properly centered
    hereby undoing my shoutout to c1b0rg0101

--- hexsmith-godot/characters/player/player_controller.gd
+++ hexsmith-godot/characters/player/player_controller.gd
@@ -4,6 +4,10 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var body_sprite: AnimatedSprite2D = $BodySprite
+@onready var hair_sprite: AnimatedSprite2D = $HairSprite
+@onready var shirt_sprite: AnimatedSprite2D = $ShirtSprite
+@onready var trouser_sprite: AnimatedSprite2D = $TrouserSprite
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,9 +18,21 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("overworld_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("overworld_move_left", "overworld_move_right")
+	
+	#Sprite Animation
+	if direction < 0:
+		body_sprite.flip_h = false
+		hair_sprite.flip_h = false
+		shirt_sprite.flip_h = false
+		trouser_sprite.flip_h = false
+	elif direction > 0:
+		body_sprite.flip_h = true
+		hair_sprite.flip_h = true
+		shirt_sprite.flip_h = true
+		trouser_sprite.flip_h = true
+		
+	# Movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
